9fd9ae75fe72004c17027d77cebdbd1d09bdb18f
Pratik <khotpratik625@gmail.com>
2026-09-05 20:01:56 +0530

    modified eks module

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -1,16 +1,30 @@
-output "cluster_connection" {
+output "eks_cluster_connection" {
   description = "Command to update kubeconfig for the EKS cluster."
   value       = "aws eks update-kubeconfig --name ${aws_eks_cluster.this.name} --region ${var.region}"
 }
 
-output "cluster_arn" {
+output "eks_cluster_arn" {
   description = "ARN of cluster"
   value       = aws_eks_cluster.this.arn
 
 }
 
-output "cluster_sgs" {
+output "eks_cluster_sgs" {
   description = "Cluster security groups"
   value       = aws_eks_cluster.this.vpc_config[0].security_group_ids
 
+}
+
+output "eks_cluster_endpoint" {
+  description = "cluster endpoint"
+  value       = aws_eks_cluster.this.endpoint
+
+}
+
+output "eks_cluster_certificate_authority_data" {
+  value = aws_eks_cluster.this.certificate_authority[0].data
+}
+
+output "eks_cluster_name" {
+  value = aws_eks_cluster.this.name
 }
