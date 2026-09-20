d07ecb1e0cc069a222a1366b219cb043eecf09c7
Pratik <khotpratik625@gmail.com>
2026-09-20 23:49:57 +0530

    fix: Changed trust policy of cluster role

--- modules/eks/iam.tf
+++ modules/eks/iam.tf
@@ -10,7 +10,10 @@ resource "aws_iam_role" "eks_cluster" {
     Version = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole"
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
         Effect = "Allow"
         Principal = {
           Service = "eks.amazonaws.com"
